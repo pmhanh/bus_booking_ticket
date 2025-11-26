a8c4c03bcd6bc8dd2af703bacc58633c719a2d35
pmhanh <150933678+pmhanh@users.noreply.github.com>
2025-11-27 00:03:34 +0700

    fix cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,6 +11,11 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  app.enableCors({
+  origin: ['https://bus-booking-ticket-1.onrender.com', 'http://localhost:5173'],
+  credentials: true,
+});
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
